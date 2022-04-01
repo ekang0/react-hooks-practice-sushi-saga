036e6c8d09a20f61ec30d9d1fa08c367e3b571f3
Esther Kang <eskang87@gmail.com>
2022-04-01 11:47:55 -0700

    commit tree 4b3f18f7cefc1eb76a1e8728f3ba188852ec739e

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushiList, setSushiList] = useState([]);
+  const [wallet, setWallet] = useState(50);
+
+  useEffect(() => {
+    fetch(API)
+    .then(resp => resp.json())
+    .then(data => setSushiList(data))
+  }, []);
+
+  function onHandleEatSushi(eatenSushi){
+    //console.log("onHandleEatSushi WORKS", eatenSushi);
+    if(wallet >= eatenSushi.price){
+      const updatedSushis = sushiList.map((sushi) => {
+        if(sushi.id === eatenSushi.id) {
+          return {...sushi, eaten: true}
+        } else {
+          return sushi
+        }
+      });
+      setSushiList(updatedSushis);
+      setWallet(wallet - eatenSushi.price);
+    } else {
+      alert("NOT ENOUGH MONEY")
+    }
+  };
+
+  const platesEaten = sushiList.filter((sushi) => sushi.eaten === true);
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushis={sushiList} handleEatSushi={onHandleEatSushi}/>
+      <Table wallet={wallet} plates={platesEaten}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer( { sushis, handleEatSushi}) {
+//console.log(sushis);
+  const [sushiIndex, setSushiIndex] = useState(0);
+
+  const sushiPieces = sushis.slice(sushiIndex, sushiIndex + 4).map((sushi) => ( <Sushi key={sushi.id} sushi={sushi} handleEatSushi={handleEatSushi}/> ));
+
+  function onHandleMoreButton() {
+    //console.log("handleMoreButton is working");
+    setSushiIndex(sushiIndex + 4);
+  };
+
   return (
     <div className="belt">
       {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiPieces}
+      <MoreButton handleMoreButton={onHandleMoreButton}/>
     </div>
   );
 }
